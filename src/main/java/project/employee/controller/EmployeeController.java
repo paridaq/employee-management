3a4f474044e7592a75a95312d6a86daa7251d3d1
Biswajit <paridabiswa2k@gmail.com>
2025-08-14 23:29:34 +0530

    adding get method

--- src/main/java/project/employee/controller/EmployeeController.java
+++ src/main/java/project/employee/controller/EmployeeController.java
@@ -2,10 +2,8 @@ package project.employee.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import project.employee.entity.Employee;
 import project.employee.service.EmployeeService;
 
@@ -18,6 +16,11 @@ public class EmployeeController {
     private final EmployeeService employeeService;
      @PostMapping("/employee")
     public Employee postEmployee(@RequestBody Employee employee){
-        return employeeService.postEmployee(employee);
+
+         return employeeService.postEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public ResponseEntity<Employee> getEmployee(){
+         return
     }
 }
